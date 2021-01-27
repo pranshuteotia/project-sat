7c0047c7c468839f7322998f8dbf90b32e5017db
Andrew Hughes <ahughes6@buffalo.edu>
2021-01-27 14:54:46 -0500

    update(Clause): Added undo stack and modifier wrappers to Clause class.

--- obj/Clause.h
+++ obj/Clause.h
@@ -7,13 +7,30 @@
 
 #include <unordered_set>
 #include <vector>
+#include <stack>
 
 struct Clause {
     std::unordered_set<int> _literals;
+    std::stack<std::unordered_set<int>> _undoStack;
     size_t _id;
+
+    void removeLiteral(int literal) {
+        if (_literals.count(literal) > 0) {
+            _undoStack.emplace(_literals);
+            _literals.erase(literal);
+        }
+    }
+
+    void clearClause() {
+        if (!_literals.empty()) {
+            _undoStack.emplace(_literals);
+            _literals.clear();
+        }
+    }
+
     int size() const { return _literals.size(); }
 
-    bool operator==(const Clause &c) const {
+    bool operator==(const Clause& c) const {
         return c._id == this->_id;
     }
 };
